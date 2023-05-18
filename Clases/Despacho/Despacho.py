2fce70b65882c38c01827599abb001d38ef85956
Francesca Massollo <franchemassollo@gmail.com>
2023-05-18 19:22:44 -0300

    Arreglados errores menores de función obtener_precio

--- Clases/Despacho/Despacho.py
+++ Clases/Despacho/Despacho.py
@@ -229,47 +229,6 @@ class Despacho:
           return max_km    
        except SinUnicaCargaExcpetion as e:
                print(f"Error {e.get_code()} / {e.get_mensaje}")
-
-    def obtenerPrecio(self, container,idC,idB, distancia):
-        container = self.__containers[idC]
-        distancia = self.__barcos[idB].get_km_Total()
-
-        if not self.__barcos:
-            raise NoListaBarcosException("No hay una lista de barcos para realizar la operacion", 123)
-    
-        if container.hayEspacio() == False:
-            precio_base = self.__calcularPrecioBase(distancia)
-        else:
-            peso_total = container.get_peso_actual()
-            precio_base = self.__calcularPrecioBase(distancia, peso_total)
-        
-        if self.__retiro == True:
-            precio_base+=20000
-
-        return precio_base 
-
-    def __calcularPrecioBase(self, distancia, peso_total=None):
-        if distancia < 100:
-            if peso_total is None:
-                return 200000
-            else:
-                return 1000 * (peso_total // 100)
-        elif distancia < 1000:
-        
-            if peso_total is None:
-                return 210000
-            else:
-                return 1100 * (peso_total // 100)
-        elif distancia < 10000:
-            if peso_total is None:
-                return 230000
-            else:
-             return 1150 * (peso_total // 100)
-        else:
-            if peso_total is None:
-                return 250000
-            else:
-                return 1500 * (peso_total // 100)
             
     def containersUnicaCarga(self):
         containers = {}
