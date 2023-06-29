ab1642a8401e680807e022b6842d48c865cc55d5
LMontes94 <monteslucas1994@gmail.com>
2023-06-29 16:29:14 -0300

    test_viajar arreglado

--- Test/SistemaPropulsion_test.py
+++ Test/SistemaPropulsion_test.py
@@ -1,4 +1,4 @@
-from unittest import TestCase 
+from unittest import TestCase
 from unittest.mock import Mock
 
 from Clases.Barco.SistemaPropulsion.Motor import Motor
@@ -10,22 +10,21 @@ from Clases.Viaje.Viaje import Viaje
 
 
 class SistemaPropulsion(TestCase):
-    
-    def test_gastar_combustible_Vela(self):
-        
-        vela=Vela()
-        tiempo=10
 
-        barco=BarcoBasico()
+    def test_gastar_combustible_Vela(self):
+
+        vela = Vela()
+        tiempo = 10
+
+        barco = BarcoBasico()
 
         barco.set_sistema_Propulsion(vela)
-        resultado=barco.get_sistema_Propulsion().gastar_combustible(tiempo)
+        resultado = barco.get_sistema_Propulsion().gastar_combustible(tiempo)
 
-        self.assertEqual(resultado,0)
-
+        self.assertEqual(resultado, 0)
 
     def test_gastar_combustible_Motor(self):
-        
+
         motor = Motor()
         tiempo = 10
 
@@ -33,8 +32,8 @@ class SistemaPropulsion(TestCase):
         barco.set_sistema_Propulsion(motor)
 
         resultado = barco.get_sistema_Propulsion().gastar_combustible(tiempo)
-        
-        self.assertEqual(resultado,60)
+
+        self.assertEqual(resultado, 60)
 
     def test_viajar(self):
 
@@ -43,8 +42,9 @@ class SistemaPropulsion(TestCase):
         viaje = Viaje()
 
         gps = Mock()
-        gps.calcularDistancia.return_value = 500 #distancia entre sede y sede en km
-        gps.calcularTiempoDeViaje.return_value = 12 #hrs de viaje TOTALES --> probar con numero > 16 para la excepcion
+        gps.calcularDistancia.return_value = 500  # distancia entre sede y sede en km
+        # hrs de viaje TOTALES --> probar con numero > 16 para la excepcion
+        gps.calcularTiempoDeViaje.return_value = 12
 
         viaje.set_sede_inicial("sedeNro1")
         viaje.set_sede_final("sedeDestinoNro2")
@@ -52,15 +52,14 @@ class SistemaPropulsion(TestCase):
         viaje.set_horas(gps.calcularTiempoDeViaje())
 
         barco.set_viaje(viaje)
-        #self.assertEquals(barco.get_viaje().get_horas(), 12) me da que el barco tiene una clase viaje con un tiempo de 12 hrs
-        
+        # self.assertEquals(barco.get_viaje().get_horas(), 12) me da que el barco tiene una clase viaje con un tiempo de 12 hrs
+
         vela = Vela()
         motor = Motor()
-        
+
         barco.set_sistema_Propulsion(motor)
-        
+        barco.set_combustible_Actual(100)
         barco.viajar()
-        self.assertEqual(barco.get_combustible_Actual(), 28) #compruebo que se gasto y actualizo el combustible 
 
-
-
+        # compruebo que se gasto y actualizo el combustible
+        self.assertEqual(barco.get_combustible_Actual(), 28)
